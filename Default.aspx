8a78abff32cf6bbacdd5a4fccacb6f58ba3f0ebf
unknown <suleymanbayazitt@gmail.com>
2020-01-06 01:41:46 +0300

    projemicin

---
+++ Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Panel ID="Panel1" runat="server" GroupingText="1 ile 100 arasında bir sayı girin...">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Sorgula..." />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Tekrar Oyna..." />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </asp:Panel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    static Random r=new Random();
+    static int tut,hak=0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            tut = r.Next(1, 101);
+            TextBox1.Focus();
+            
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Button2.Enabled = false;
+        hak++;
+        int sayi = Convert.ToInt32(TextBox1.Text);
+        if (sayi<tut)
+        {
+            Label1.Text = "Sayınızı Büyütün...";
+            TextBox1.Text = "";
+            TextBox1.Focus();
+        }
+        else if (sayi > tut)
+        {
+            Label1.Text = "Sayınızı Küçültün...";
+            TextBox1.Text = "";
+            TextBox1.Focus();
+        }
+        else if (sayi == tut)
+        {
+            Label1.Text = "Tebrikler..."+hak.ToString()+" .hakkınızda bildiniz";
+            Button2.Enabled = true;
+            Button1.Enabled = false;    
+        }
+        Label2.Text = (10 - hak).ToString() + " hakkınız kaldı...";
+        if (hak==10)
+        {
+            Label2.Text = tut.ToString() + "olan sayımı bilemedin...";
+            Button1.Enabled = false;
+            Button2.Enabled = true;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        tut = r.Next(1,101);
+        hak = 0;
+        TextBox1.Text = "";
+        TextBox1.Focus();
+        Button1.Enabled = true;
+        Label1.Text = "";
+        Label2.Text = "10 hakkınız var";
+        Button2.Enabled = false;
+    }
+}
